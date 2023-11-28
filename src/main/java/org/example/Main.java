7530a0b777d439fe767d519b869ed80ac6461b51
Sebastian Giraldo <sebastianberrio0627@gmail.com>
2023-11-28 11:30:07 -0500

    se inicia proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Natillera la anuelita toretto
+        //¿Qué necesito para procesar la natillera de 1 cliente
+        //ENTRADAS
+        int identificadorCliente=233444;
+        long documentoIdentidad=100223456754l;
+        byte edadCliente=34;
+        short cuotaMensualCliente=25000;
+        float tasaInteresFijo=0.1f;
+        char CalificacionCliente='a';
+    }
+}
